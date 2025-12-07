6f9f56efa40e85d08e9b11f62fc1b647195179a2
Ruan Valença <ruangobira@gmail.com>
2025-12-07 08:40:36 -0300

    chore: ajusta configuração para banco de dados

--- BackEnd/src/db.ts
+++ BackEnd/src/db.ts
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "delishare",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT) || 5432,
-  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false },
 });
--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -1,18 +1,17 @@
-import http from "http";
-import dotenv from "dotenv";
 import app from "./app";
+import http from "http";
 import { pool } from "./db";
 
-dotenv.config();
-
 const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
 
 pool
   .query("SELECT NOW()")
   .then(() => console.log("✅ Supabase conectado"))
-  .catch((err) => console.error("❌ Erro ao conectar no Supabase", err));
-
-const server = http.createServer(app);
+  .catch((err: string) =>
+    console.error("❌ Erro ao conectar no Supabase", err)
+  );
 
 server.listen(PORT, () => {
   console.log(`✅ Servidor rodando na porta ${PORT}`);
