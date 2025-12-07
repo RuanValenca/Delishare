ec501ec63ee7b8d4e4dba1f642e9b58d17defcdc
Ruan Valença <ruangobira@gmail.com>
2025-12-06 23:25:34 -0300

    chore: atualiza tsconfig.json e yarn.lock, adiciona conexão com Supabase no servidor

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -1,9 +1,17 @@
 import app from "./app";
 import http from "http";
+import { pool } from "./db";
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+
+pool
+  .query("SELECT NOW()")
+  .then(() => console.log("✅ Supabase conectado"))
+  .catch((err: string) =>
+    console.error("❌ Erro ao conectar no Supabase", err)
+  );
 
 server.listen(PORT, () => {
   console.log(`✅ Servidor rodando na porta ${PORT}`);
