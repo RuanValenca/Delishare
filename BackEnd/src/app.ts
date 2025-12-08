fbd0290b69c7ba03ba7cd2d001d6e22c7f27cc30
Ruan Valença <ruangobira@gmail.com>
2025-12-08 20:05:36 -0300

    correção de origin para cors

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -16,14 +16,12 @@ if (process.env.NODE_ENV !== "production") {
 const app = express();
 
 // Configuração do CORS
-const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "https://delishare-app.netlify.app",
+    credentials: true,
+  })
+);
 
 // Logs e parsing
 app.use(morgan("dev"));
